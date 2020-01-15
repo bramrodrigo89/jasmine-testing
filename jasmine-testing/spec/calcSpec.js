68224bc30c7d55d5e3ea7609b5a6b5bb4751346e
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-01-15 21:43:02 +0000

    created framework for Jasmine and tested a simple function calculator

---
+++ jasmine-testing/spec/calcSpec.js
@@ -0,0 +1,15 @@
+describe('Calculator', function(){
+    describe('Addition tests', function(){
+        it('should return 42',function(){
+           expect(addition(20,22)).toBe(42);
+        });
+        it('should return 26',function(){
+            expect(addition(17,9)).toBe(26);
+        });
+        it(`should return an error if we don't supply two numbers `,function(){
+            spyOn(window, 'alert');
+            addition('word1','word2');
+            expect(window.alert).toHaveBeenCalledWith("Error!");
+        }); 
+    }); 
+});
